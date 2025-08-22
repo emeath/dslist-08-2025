119ac9d49fed86b9464294607e902569fe21b369
Matheus <matheus.martins.jeronimo@gmail.com>
2025-08-22 17:07:19 -0300

    GET game lists

---
+++ src/main/java/com/matheus/dslist/dto/GameListDTO.java
@@ -0,0 +1,26 @@
+package com.matheus.dslist.dto;
+
+import com.matheus.dslist.entities.GameList;
+
+public class GameListDTO {
+	
+	private Long id;
+	private String name;
+	
+	public GameListDTO() {
+		
+	}
+	
+	public GameListDTO(GameList entity) {
+		id = entity.getId();
+		name = entity.getName();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
---
+++ src/main/java/com/matheus/dslist/services/GameListService.java
@@ -0,0 +1,24 @@
+package com.matheus.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.matheus.dslist.dto.GameListDTO;
+import com.matheus.dslist.entities.GameList;
+import com.matheus.dslist.repositories.GameListRepository;
+
+@Service
+public class GameListService {
+
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll() {
+		List<GameList> result = gameListRepository.findAll();
+		return result.stream().map(x -> new GameListDTO(x)).toList();
+	}
+}
